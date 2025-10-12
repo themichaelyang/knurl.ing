99411f68e8c0630c86aaccb545f8f4373e8bad8f
Michael Yang <themichaelyang@gmail.com>
2025-10-12 16:13:36 -0400

    swap font

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,23 @@ import index from './index.html'
 
 // const db = new Database(LocalConfig.database.path)
 
+// Bun seems like it bundles the fonts?
+// const STATIC_FILES = [
+  // "/static/routed-gothic/RoutedGothicWide.woff",
+  // "/static/routed-gothic/RoutedGothicWide.woff2"
+// ]
+
+let routes: Record<string, any> = {
+  "/": index
+}
+
+// STATIC_FILES.map(async (path) => {
+//   routes[path] = new Response(await Bun.file(process.cwd() + path).bytes())
+// })
+
 Bun.serve({
-  routes: {
-    "/": index,
-    "/static/fonts/iAWriterDuoS-Regular.woff2": new Response(Bun.file("./static/fonts/iAWriterDuoS-Regular.woff2")),
-    "/static/fonts/iAWriterDuoS-Bold.woff2": new Response(Bun.file("./static/fonts/iAWriterDuoS-Bold.woff2"))
-  },
+  routes: routes,
   port: 2000
 })
+
+console.log()
